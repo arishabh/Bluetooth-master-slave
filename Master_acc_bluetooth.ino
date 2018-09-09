e0df7fb858e389764f956db3e539e2cfc30ab410
Rishabh Agrawal <43053152+arishabh@users.noreply.github.com>
2018-09-08 22:37:20 -0400

    Add files via upload

---
+++ Master_acc_bluetooth.ino
@@ -0,0 +1,122 @@
+float val = 0;
+int mot11 = 3;
+int mot12 = 4;
+int mot21 = 5;
+int mot22 = 6;
+int en1 = 9;
+int en2 = 10;
+
+
+void setup() {
+  Serial.begin(38400);
+  pinMode(mot11, OUTPUT);
+  pinMode(mot12, OUTPUT);
+  pinMode(mot22, OUTPUT);
+  pinMode(mot21, OUTPUT);
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+}
+
+void forward(){
+  digitalWrite(mot11, HIGH);
+  digitalWrite(mot12, LOW);
+  digitalWrite(mot21, HIGH);
+  digitalWrite(mot22, LOW);
+  Serial.print("Forward");
+}
+
+void right(){
+  digitalWrite(mot11, HIGH);
+  digitalWrite(mot12, LOW);
+  digitalWrite(mot21, LOW);
+  digitalWrite(mot22, LOW);
+  Serial.print("Right");
+}
+
+void left(){
+  digitalWrite(mot11, LOW);
+  digitalWrite(mot12, LOW);
+  digitalWrite(mot21, HIGH);
+  digitalWrite(mot22, LOW);
+  Serial.print("Left");
+}
+
+void back(){
+  digitalWrite(mot11, LOW);
+  digitalWrite(mot12, HIGH);
+  digitalWrite(mot21, LOW);
+  digitalWrite(mot22, HIGH);
+  Serial.print("Back");
+}
+
+void stay(){
+  digitalWrite(mot11, LOW);
+  digitalWrite(mot12, LOW);
+  digitalWrite(mot21, LOW);
+  digitalWrite(mot22, LOW);
+  Serial.print("Stop");
+}
+
+void loop(){ 
+  val = Serial.read();
+  if(Serial.available()){
+    Serial.println(val);
+  }
+  /*switch(val){
+    case 1:
+      stay();
+      break;
+    case 2:
+      analogWrite(en1,85);
+      analogWrite(en1,85);
+      forward();
+    case 3:
+      analogWrite(en1,170);
+      analogWrite(en1,170);
+      forward();
+    case 4:
+      analogWrite(en1,255);
+      analogWrite(en1,255);
+      forward();
+    case 5:
+      analogWrite(en1,255);
+      analogWrite(en1,255);
+      right();
+    case 6:
+      analogWrite(en1,170);
+      analogWrite(en1,170);
+      right();
+    case 7:
+      analogWrite(en1,85);
+      analogWrite(en1,85);
+      right();
+    case 8:
+      analogWrite(en1,85);
+      analogWrite(en1,85);
+      left();
+    case 9:
+      analogWrite(en1,170);
+      analogWrite(en1,170);
+      left();
+    case 10:
+      analogWrite(en1,255);
+      analogWrite(en1,255);
+      left();
+    case 11:
+      analogWrite(en1,85);
+      analogWrite(en1,85);
+      back();
+    case 12:
+      analogWrite(en1,170);
+      analogWrite(en1,170);
+      back();
+    case 13:
+      analogWrite(en1,255);
+      analogWrite(en1,255);
+      back();
+    
+  }*/
+  delay(1000);
+  Serial.print("\n");
+  
+}
---
+++ Slave_acc_bluetooth.ino
@@ -0,0 +1,88 @@
+int valx = 0;
+int valy = 0;
+int accy = A1;
+int accx = A2;
+
+
+void setup() {
+  Serial.begin(38400);
+  pinMode(accx, INPUT);
+  pinMode(accy, INPUT);
+}
+
+void loop() {
+  valx = analogRead(accx);
+  valy = analogRead(accy);
+  if (Serial.available()){
+    Serial.println(valx);
+    Serial.println(valy);
+  }
+
+  if(valx > 320 && valx < 350 && valy > 320 && valy < 350){ //stay
+    Serial.write('1');
+    Serial.print("\n");
+  }
+
+  if(valx > 355 && valx < 365 && valy > 330 && valy < 350){ //forward
+    Serial.write('2');
+    Serial.print("\n");
+  }
+
+  if(valx > 365 && valx < 375 && valy > 330 && valy < 350){ //forward
+    Serial.write('3');
+    Serial.print("\n");
+  }
+
+  if(valx > 375 && valy > 330 && valy < 350){ //forward
+    Serial.write('4');
+    Serial.print("\n");
+  }
+
+  if(valx > 330 && valx < 350 && valy > 390){ //right
+    Serial.write('5');
+    Serial.print("\n");
+  }
+
+  if(valx > 330 && valx < 350 && valy > 375 && valy < 390){ //right
+    Serial.write('6');
+    Serial.print("\n");
+  }
+
+  if(valx > 330 && valx < 350 && valy > 365 && valy < 375){ //right
+    Serial.write('7');
+    Serial.print("\n");
+  }
+
+  if(valx > 330 && valx < 350 && valy < 310 && valy > 295) { //left
+    Serial.write('8');
+    Serial.print("\n");
+  }
+  
+  if(valx > 330 && valx < 350 && valy < 295 && valy > 285) { //left
+    Serial.write('9');
+    Serial.print("\n");
+  }
+
+  if(valx > 330 && valx < 350 && valy < 285) { //left
+    Serial.write("10");
+    Serial.print("\n");
+  }
+  
+  if(valy > 330 && valy < 350 && valx < 310 && valx > 295) { //back
+    Serial.write("11");
+    Serial.print("\n");
+  }
+  
+  if(valy > 330 && valy < 350 && valx < 295 && valx > 285) { //back
+    Serial.write("12");
+    Serial.print("\n");
+  }
+
+  if(valy > 330 && valy < 350 && valx < 285) { //back
+    Serial.write("13");
+    Serial.print("\n");
+  }
+  delay(1000);
+  Serial.print("\n");
+  
+}
